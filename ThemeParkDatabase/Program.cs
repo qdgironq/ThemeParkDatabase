f08a0a9133946bf848e95f79baba2e6f72e6116e
daniel giron <38026432+qdgironq@users.noreply.github.com>
2018-04-20 00:05:58 -0500

    comment test

--- ThemeParkDatabase/Program.cs
+++ ThemeParkDatabase/Program.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Logging;
 
 namespace ThemeParkDatabase
 {
-    //some comment
+    //some comment test
     public class Program
     {
         public static void Main(string[] args)
